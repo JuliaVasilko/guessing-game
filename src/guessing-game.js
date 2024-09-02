b2a89ccbb4222893c039857170fae3ded2bb146d
Julia Vasilko <juliavasilko2015@gmail.com>
2024-09-02 20:50:50 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min;
+        this.max;
+        this.currentGuess;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentGuess = Math.ceil((this.min + this.max) / 2);
+        return this.currentGuess;
     }
 
     lower() {
-
+        this.setRange(this.min, this.currentGuess);
     }
 
     greater() {
-
+        this.setRange(this.currentGuess, this.max);
     }
 }
 
